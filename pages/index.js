384e078dea2ccd4e7bc0f702aab7e6bf2729abb0
Anna Kawecka <aakawecka89@gmail.com>
2022-07-19 14:16:58 +0200

    head, metadata

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
+import Head from 'next/head';
 
 function HomePage() {
 	const featuredEvents = getFeaturedEvents();
 	return (
 		<div>
+			<Head>
+				<title>It meets</title>
+				<meta name="description" content="Find your meetings" />
+			</Head>
 			<EventList items={featuredEvents} />
 		</div>
 	);
